d401095085b62882bb9021be8c33ba77bc22e7d3
5ubie <djsubaruu@gmail.com>
2024-01-30 14:15:04 -0500

    add v4 theme, update readme, script accordingly, fix mkdir error

--- README.md
+++ README.md
@@ -6,25 +6,34 @@ There are no dependencies apart from the above packages and their dependencies. 
 
 My system font is `IBM Plex Sans` which should be downloadable through your package manager, but is also available [here](https://github.com/IBM/plex/releases/tag/v6.4.0).
 #### List of themes:
-- **`v1-bw-brutalist`**
+Names generally follow the syntax of `v[#]-[color]-[style]`.
+- **`v1-bw-brutalist`** (Dec 2023)
 
 	![v1 zoom screenshot](https://github.com/5ubie/sway-configs/blob/main/assets/v1-screenshot-zoom.png?raw=true)
 	
 	- A utilitarian, opaque black and white scheme with sharp, square edges
 	- Can be used with both `swaylock` and `swaylock-effects`
-- **`v2-bw-softened`**
+- **`v2-bw-softened`** (Jan 2024)
 
 	![v2 zoom screenshot](https://github.com/5ubie/sway-configs/blob/main/assets/v2-screenshot-zoom.png?raw=true)
 	
 	- A friendlier black and white scheme with transparency, rounded edges and some use of blur
 	- Requires `swaylock-effects` for screen blur and clock
 		- To use with `swaylock`, remove lines `1`-`14` and set a background image or color of your choosing
-- **`v3-light-minimal`**
+- **`v3-light-minimal`** (Jan 2024)
 	
 	![v3 zoom screenshot](https://github.com/5ubie/sway-configs/blob/main/assets/v3-screenshot-zoom.png?raw=true)
 	
 	- A minimal, flat and mostly white scheme with subtle highlighting and transparency throughout
 	- Requires `swaylock-effects` for screen blur and clock
+		- To use with `swaylock`, remove lines `1`-`14` ...
+- **`v4-dark-minimal`** (Jan 2024)
+	
+	![v4 zoom screenshot](https://github.com/5ubie/sway-configs/blob/main/assets/v4-screenshot-zoom.png?raw=true)
+	
+	- A minimal, sleek dark scheme with subtle gradient highlights and transparency throughout
+	- Requires `swaylock-effects` for screen blur and clock
+		- To use with `swaylock`, remove lines `1`-`14` ...
 ## Usage
 Themes can be managed with the `config-update.sh` bash script or manually copied into default config folders.
 
@@ -46,9 +55,10 @@ Single digit options (i.e. `1` or `2`) were made to make managing this repositor
 - I've tested all this on a laptop running Fedora 39, things might look different on different setups
 
 ### More Screenshots
-| `v1-bw-brutalist` | ![v1 full screenshot](https://github.com/5ubie/sway-configs/blob/main/assets/v1-screenshot.png?raw=true)![v1 swaylock pic](https://github.com/5ubie/sway-configs/blob/main/assets/v1-swaylock-pic.jpg?raw=true) |  |
-| ---- | ---- | ---- |
-| **`v2-bw-softened`** | ![v2 full screenshot](https://github.com/5ubie/sway-configs/blob/main/assets/v2-screenshot.png?raw=true)![v2 swaylock pic](https://github.com/5ubie/sway-configs/blob/main/assets/v2-swaylock-pic.jpg?raw=true) |  |
-| **`v3-light-minimal`** | ![v3 full screenshot](https://github.com/5ubie/sway-configs/blob/main/assets/v3-screenshot.png?raw=true) |  |
+| `v1-bw-brutalist` | ![v1 full screenshot](https://github.com/5ubie/sway-configs/blob/main/assets/v1-screenshot.png?raw=true)![v1 swaylock pic](https://github.com/5ubie/sway-configs/blob/main/assets/v1-swaylock-pic.jpg?raw=true) |
+| ---- | ---- |
+| **`v2-bw-softened`** | ![v2 full screenshot](https://github.com/5ubie/sway-configs/blob/main/assets/v2-screenshot.png?raw=true)![v2 swaylock pic](https://github.com/5ubie/sway-configs/blob/main/assets/v2-swaylock-pic.jpg?raw=true) |
+| **`v3-light-minimal`** | ![v3 full screenshot](https://github.com/5ubie/sway-configs/blob/main/assets/v3-screenshot.png?raw=true) |
+| **`v4-dark-minimal`** | ![v4 full screenshot](https://github.com/5ubie/sway-configs/blob/main/assets/v4-screenshot.png?raw=true) |
 
 (I'm not smart enough to actually get a screenshot of swaylock lol)
--- config-update.sh
+++ config-update.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # grab date and time for backup directory naming
 dtstr=$(date +"%Y-%m-%d_%H-%M-%S")
-
 # display prompt
-printf "\nSelect an option:\n-----------------\n[1]  Copy current configs to 'v1' repo folder\n[2]  Copy current configs to 'v2' repo folder\n[3]  Copy current configs to 'v3' repo folder\n[10] Copy & load 'v1' config to system\n[20] Copy & load 'v2' config to system\n[30] Copy & load 'v3' config to system\n\n[e]  Exit prompt\n-----------------\n> "
+printf "\nSelect an option:\n-----------------\n[1]  Copy current configs to 'v1' repo folder\n[2]  Copy current configs to 'v2' repo folder\n[3]  Copy current configs to 'v3' repo folder\n[4]  Copy current configs to 'v4' repo folder\n[10] Copy & load 'v1' config to system\n[20] Copy & load 'v2' config to system\n[30] Copy & load 'v3' config to system\n[40] Copy & load 'v4' config to system\n\n[e]  Exit prompt\n-----------------\n> "
 read choice
 # if-else tree
 if [ $choice == "1" ]; then
@@ -51,23 +50,39 @@ elif [ $choice == "3" ]; then
 	elif [ $confv2 == "N" ]; then
 		echo "Cancelled."
 	fi
+elif [ $choice == "4" ]; then
+	# prompt confirmation of overwrite
+	printf "Confirm to overwrite all contents in sway-configs/v3-light-minimal/ [y/N]\n> "
+	read confv4
+	if [ $confv4 == "y" ]; then
+		# copy current loaded configs to repo directory
+		sudo cp /etc/xdg/dunst/dunstrc "v4-dark-minimal/dunst/"
+		sudo cp /etc/xdg/waybar/config "v4-dark-minimal/waybar/"
+		sudo cp /etc/xdg/waybar/style.css "v4-dark-minimal/waybar/"
+		sudo cp ~/.config/sway/config "v4-dark-minimal/sway/"
+		sudo cp ~/.config/swaylock/config "v4-dark-minimal/swaylock/"
+		echo "All 5 config files copied to sway-configs/v4-dark-minimal/"
+	elif [ $confv2 == "N" ]; then
+		echo "Cancelled."
+	fi
 elif [ $choice == "10" ]; then
 	# make backup directory
-	mkdir "backup-[$dtstr]/"
-	mkdir "backup-[$dtstr]/dunst/"
-	mkdir "backup-[$dtstr]/waybar/"
-	mkdir "backup-[$dtstr]/sway/"
-	mkdir "backup-[$dtstr]/swaylock/"
+	mkdir -p "backups/"
+	mkdir -p "backups/backup-[$dtstr]/"
+	mkdir -p "backups/backup-[$dtstr]/dunst/"
+	mkdir -p "backups/backup-[$dtstr]/waybar/"
+	mkdir -p "backups/backup-[$dtstr]/sway/"
+	mkdir -p "backups/backup-[$dtstr]/swaylock/"
 	# copy root config files to backup
-	sudo cp /etc/xdg/dunst/dunstrc "backup-[$dtstr]/dunst/"
-	sudo cp /etc/xdg/waybar/config "backup-[$dtstr]/waybar/"
-	sudo cp /etc/xdg/waybar/style.css "backup-[$dtstr]/waybar/"
-	sudo cp /etc/sway/config "backup-[$dtstr]/sway/config-[_etc_sway]"
-	sudo cp /etc/swaylock/config "backup-[$dtstr]/swaylock/config-[_etc_swaylock]"
+	sudo cp /etc/xdg/dunst/dunstrc "backups/backup-[$dtstr]/dunst/"
+	sudo cp /etc/xdg/waybar/config "backups/backup-[$dtstr]/waybar/"
+	sudo cp /etc/xdg/waybar/style.css "backups/backup-[$dtstr]/waybar/"
+	sudo cp /etc/sway/config "backups/backup-[$dtstr]/sway/config-[_etc_sway]"
+	sudo cp /etc/swaylock/config "backups/backup-[$dtstr]/swaylock/config-[_etc_swaylock]"
 	# copy user config files to backup
-	sudo cp ~/.config/sway/config "backup-[$dtstr]/sway/"
-	sudo cp ~/.config/swaylock/config "backup-[$dtstr]/swaylock/"
-	echo "Existing config files copied to sway-configs/backup-[$dtstr]/"
+	sudo cp ~/.config/sway/config "backups/backup-[$dtstr]/sway/"
+	sudo cp ~/.config/swaylock/config "backups/backup-[$dtstr]/swaylock/"
+	echo "Existing config files copied to sway-configs/backups/backup-[$dtstr]/"
 	
 	# copy v1-bw-brutalist files to system
 	sudo cp v1-bw-brutalist/dunst/dunstrc /etc/xdg/dunst/
@@ -79,24 +94,27 @@ elif [ $choice == "10" ]; then
 	# reload wm
 	echo "Reloading sway..."
 	swaymsg reload
+	pkill dunst
 	echo "Configs have been applied."
 elif [ $choice == "20" ]; then
 	# make backup directory
-	mkdir "backup-[$dtstr]/"
-	mkdir "backup-[$dtstr]/dunst/"
-	mkdir "backup-[$dtstr]/waybar/"
-	mkdir "backup-[$dtstr]/sway/"
-	mkdir "backup-[$dtstr]/swaylock/"
+	mkdir -p "backups/"
+	mkdir -p "backups/backup-[$dtstr]/"
+	mkdir -p "backups/backup-[$dtstr]/dunst/"
+	mkdir -p "backups/backup-[$dtstr]/waybar/"
+	mkdir -p "backups/backup-[$dtstr]/sway/"
+	mkdir -p "backups/backup-[$dtstr]/swaylock/"
 	# copy root config files to backup
-	sudo cp /etc/xdg/dunst/dunstrc "backup-[$dtstr]/dunst/"
-	sudo cp /etc/xdg/waybar/config "backup-[$dtstr]/waybar/"
-	sudo cp /etc/xdg/waybar/style.css "backup-[$dtstr]/waybar/"
-	sudo cp /etc/sway/config "backup-[$dtstr]/sway/config-[_etc_sway]"
-	sudo cp /etc/swaylock/config "backup-[$dtstr]/swaylock/config-[_etc_swaylock]"
+	sudo cp /etc/xdg/dunst/dunstrc "backups/backup-[$dtstr]/dunst/"
+	sudo cp /etc/xdg/waybar/config "backups/backup-[$dtstr]/waybar/"
+	sudo cp /etc/xdg/waybar/style.css "backups/backup-[$dtstr]/waybar/"
+	sudo cp /etc/sway/config "backups/backup-[$dtstr]/sway/config-[_etc_sway]"
+	sudo cp /etc/swaylock/config "backups/backup-[$dtstr]/swaylock/config-[_etc_swaylock]"
 	# copy user config files to backup
-	sudo cp ~/.config/sway/config "backup-[$dtstr]/sway/"
-	sudo cp ~/.config/swaylock/config "backup-[$dtstr]/swaylock/"
-	echo "Existing config files copied to sway-config/backup-[$dtstr]/"
+	sudo cp ~/.config/sway/config "backups/backup-[$dtstr]/sway/"
+	sudo cp ~/.config/swaylock/config "backups/backup-[$dtstr]/swaylock/"
+	echo "Existing config files copied to sway-configs/backups/backup-[$dtstr]/"
+
 	# copy v2-bw-softened files to system
 	sudo cp v2-bw-softened/dunst/dunstrc /etc/xdg/dunst/
 	sudo cp v2-bw-softened/waybar/config /etc/xdg/waybar/
@@ -105,25 +123,28 @@ elif [ $choice == "20" ]; then
 	sudo cp v2-bw-softened/swaylock/config ~/.config/swaylock/
 	echo "All 'v2-bw-softened' config files copied to default config locations.\nReloading sway..."
 	swaymsg reload
+	pkill dunst
 	echo "Configs have been applied."
 elif [ $choice == "30" ]; then
 	# make backup directory
-	mkdir "backup-[$dtstr]/"
-	mkdir "backup-[$dtstr]/dunst/"
-	mkdir "backup-[$dtstr]/waybar/"
-	mkdir "backup-[$dtstr]/sway/"
-	mkdir "backup-[$dtstr]/swaylock/"
+	mkdir -p "backups/"
+	mkdir -p "backups/backup-[$dtstr]/"
+	mkdir -p "backups/backup-[$dtstr]/dunst/"
+	mkdir -p "backups/backup-[$dtstr]/waybar/"
+	mkdir -p "backups/backup-[$dtstr]/sway/"
+	mkdir -p "backups/backup-[$dtstr]/swaylock/"
 	# copy root config files to backup
-	sudo cp /etc/xdg/dunst/dunstrc "backup-[$dtstr]/dunst/"
-	sudo cp /etc/xdg/waybar/config "backup-[$dtstr]/waybar/"
-	sudo cp /etc/xdg/waybar/style.css "backup-[$dtstr]/waybar/"
-	sudo cp /etc/sway/config "backup-[$dtstr]/sway/config-[_etc_sway]"
-	sudo cp /etc/swaylock/config "backup-[$dtstr]/swaylock/config-[_etc_swaylock]"
+	sudo cp /etc/xdg/dunst/dunstrc "backups/backup-[$dtstr]/dunst/"
+	sudo cp /etc/xdg/waybar/config "backups/backup-[$dtstr]/waybar/"
+	sudo cp /etc/xdg/waybar/style.css "backups/backup-[$dtstr]/waybar/"
+	sudo cp /etc/sway/config "backups/backup-[$dtstr]/sway/config-[_etc_sway]"
+	sudo cp /etc/swaylock/config "backups/backup-[$dtstr]/swaylock/config-[_etc_swaylock]"
 	# copy user config files to backup
-	sudo cp ~/.config/sway/config "backup-[$dtstr]/sway/"
-	sudo cp ~/.config/swaylock/config "backup-[$dtstr]/swaylock/"
-	echo "Existing config files copied to sway-config/backup-[$dtstr]/"
-	# copy v2-bw-softened files to system
+	sudo cp ~/.config/sway/config "backups/backup-[$dtstr]/sway/"
+	sudo cp ~/.config/swaylock/config "backups/backup-[$dtstr]/swaylock/"
+	echo "Existing config files copied to sway-configs/backups/backup-[$dtstr]/"
+
+	# copy v3-light-minimal files to system
 	sudo cp v3-light-minimal/dunst/dunstrc /etc/xdg/dunst/
 	sudo cp v3-light-minimal/waybar/config /etc/xdg/waybar/
 	sudo cp v3-light-minimal/waybar/style.css /etc/xdg/waybar/
@@ -131,7 +152,38 @@ elif [ $choice == "30" ]; then
 	sudo cp v3-light-minimal/swaylock/config ~/.config/swaylock/
 	echo "All 'v3-light-minimal' config files copied to default config locations.\nReloading sway..."
 	swaymsg reload
+	pkill dunst
 	echo "Configs have been applied."
+elif [ $choice == "40" ]; then
+	# make backup directory
+	mkdir -p "backups/"
+	mkdir -p "backups/backup-[$dtstr]/"
+	mkdir -p "backups/backup-[$dtstr]/dunst/"
+	mkdir -p "backups/backup-[$dtstr]/waybar/"
+	mkdir -p "backups/backup-[$dtstr]/sway/"
+	mkdir -p "backups/backup-[$dtstr]/swaylock/"
+	# copy root config files to backup
+	sudo cp /etc/xdg/dunst/dunstrc "backups/backup-[$dtstr]/dunst/"
+	sudo cp /etc/xdg/waybar/config "backups/backup-[$dtstr]/waybar/"
+	sudo cp /etc/xdg/waybar/style.css "backups/backup-[$dtstr]/waybar/"
+	sudo cp /etc/sway/config "backups/backup-[$dtstr]/sway/config-[_etc_sway]"
+	sudo cp /etc/swaylock/config "backups/backup-[$dtstr]/swaylock/config-[_etc_swaylock]"
+	# copy user config files to backup
+	sudo cp ~/.config/sway/config "backups/backup-[$dtstr]/sway/"
+	sudo cp ~/.config/swaylock/config "backups/backup-[$dtstr]/swaylock/"
+	echo "Existing config files copied to sway-configs/backups/backup-[$dtstr]/"
+
+	# copy v4-dark-minimal files to system
+	sudo cp v4-dark-minimal/dunst/dunstrc /etc/xdg/dunst/
+	sudo cp v4-dark-minimal/waybar/config /etc/xdg/waybar/
+	sudo cp v4-dark-minimal/waybar/style.css /etc/xdg/waybar/
+	sudo cp v4-dark-minimal/sway/config ~/.config/sway/
+	sudo cp v4-dark-minimal/swaylock/config ~/.config/swaylock/
+	echo "All 'v4-dark-minimal' config files copied to default config locations.\nReloading sway..."
+	swaymsg reload
+	pkill dunst
+	echo "Configs have been applied."
+
 elif [ $choice == "e" ]; then
 	echo "Exiting..."
 fi
